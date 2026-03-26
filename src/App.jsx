9468a70f1e32a2fbc37fe58929d31fb1c065a815
William Hou <williamhou38@yahoo.com>
2026-03-26 07:32:12 -0500

    new changes

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import TabButton from './components/TabButton/TabButton.jsx';
 import { EXAMPLES } from './data.js';
 
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+const buttonNames = ['components', 'jsx', 'props', 'state'];
 
 function genRandomInt(max) {
   return Math.floor(Math.random() * (max + 1));
@@ -21,6 +22,7 @@ function App() {
   function handleSelect(selectedButton) {
     setSelectedTopic(selectedButton);
   }
+
   const topic = EXAMPLES[selectedTopic];
   let tabContent = <p>Please select a topic</p>;
 
@@ -42,27 +44,19 @@ function App() {
       <h1>{description} Vite + React</h1>
       <section id="core-concepts">
         <ul className="items">
-          <CoreConcept title={CORE_CONCEPTS[0].title} description={CORE_CONCEPTS[0].description} image={CORE_CONCEPTS[0].image}></CoreConcept>
-          <CoreConcept {...CORE_CONCEPTS[1]}></CoreConcept>
-          <CoreConcept {...CORE_CONCEPTS[2]}></CoreConcept>
-          <CoreConcept {...CORE_CONCEPTS[3]}></CoreConcept>
+          {CORE_CONCEPTS.map((conceptItem) => (
+            <CoreConcept key={conceptItem.title} {...conceptItem} />
+          ))}
         </ul>
       </section>
       <section id="examples">
         <h2>Examples</h2>
         <menu className="items">
-          <TabButton isSelected={selectedTopic === 'components'} onSelect={() => handleSelect('components')}>
-            Components
-          </TabButton>
-          <TabButton isSelected={selectedTopic === 'jsx'} onSelect={() => handleSelect('jsx')}>
-            JSX
-          </TabButton>
-          <TabButton isSelected={selectedTopic === 'props'} onSelect={() => handleSelect('props')}>
-            Props
-          </TabButton>
-          <TabButton isSelected={selectedTopic === 'state'} onSelect={() => handleSelect('state')}>
-            State
-          </TabButton>
+          {buttonNames.map((btnName) => (
+            <TabButton isSelected={selectedTopic === btnName} onSelect={() => handleSelect(btnName)} key={btnName}>
+              {btnName === 'jsx' ? btnName.toUpperCase() : btnName.charAt(0).toUpperCase() + btnName.slice(1)}
+            </TabButton>
+          ))}
         </menu>
         {tabContent}
       </section>
